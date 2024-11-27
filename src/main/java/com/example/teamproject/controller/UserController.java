e24af66b26c5c454072a2b564ce7bfda97c88424
choiht0904 <choiht0904@naver.com>
2024-11-27 12:45:02 +0900

    create user files
    유저 관련 파일들 생성

---
+++ src/main/java/com/example/teamproject/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.example.teamproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
---
+++ src/main/java/com/example/teamproject/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.teamproject.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+public class UserRepository {
+}
